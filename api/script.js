0901061b894b32d1c3f39b7dee14da3f96a6762b
Minin <70429604+MininxD@users.noreply.github.com>
2023-08-15 17:56:13 +0700

    Delete script.js

--- api/script.js
+++
@@ -1,7 +0,0 @@
-const output = document.getElementById("hasil");
-const masukan = document.getElementById("input");
-
-function jumlah() {
-  const hasil = masukan.value * 3600;
-  output.innerHTML = hasil;
-}
