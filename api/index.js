7bd682316b24a0c7bfdd0d43daefda3a1daffd49
Minin <70429604+MininxD@users.noreply.github.com>
2023-11-01 17:23:00 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -40,39 +40,67 @@ module.exports = async (req, res) => {
       res.json({ result: "false" });
     } else {
       //
-      //Unix Time
-      var uwib = Number(response.body) + Number(GMT7);
-      var uwita = Number(response.body) + Number(GMT8);
-      var uwit = Number(response.body) + Number(GMT9);
-      //change the plus into minus if youre on GMT-1 and up
+      //Unix Time GMT 0
+      var unixTime = Number(response.body) + Number(GMT0);
+      //convert UNIX with moment
+      var unixMoment = moment.unix(unixTime);
 
-      var wibnix = moment.unix(uwib);
-      var witanix = moment.unix(uwita);
-      var witnix = moment.unix(uwit);
+
+
+      //Unix Time WIB WITA WIT
+      var unixWib = Number(response.body) + Number(GMT7);
+      var unixWita = Number(response.body) + Number(GMT8);
+      var unixWit = Number(response.body) + Number(GMT9);
+      //Convert UNIX with moment
+      var wibMoment = moment.unix(unixWib);
+      var witaMoment = moment.unix(unixWita);
+      var witMoment = moment.unix(unixWit);
 
       //API Response
       res.json({
         docs: "https://github.com/mininxd/timeapi/",
+        UNIX: [
+          {
+            //Timezone hanyalah string
+            timezone: "0",
+
+            //Unix Time
+            unixtime: unixTime,
+            date: unixMoment.format("L"),
+            time: unixMoment.format("LTS"),
+            time12: unixMoment.format("hh:mm"),
+            time12a: unixMoment.format("hh:mm a"),
+            time24: unixMoment.format("H:mm"),
+            AMPM: unixMoment.format("a"),
+            second: unixMoment.format("ss"),
+            minute: unixMoment.format("mm"),
+            hour: unixMoment.format("hh"),
+            weekday: unixMoment.format("dddd"),
+            day: unixMoment.format("D"),
+            month: unixMoment.format("M"),
+            year: unixMoment.format("yyyy"),
+          },
+        ],
         wib: [
           {
             //Timezone hanyalah string
             timezone: "Asia/Jakarta",
 
             //Unix Time
-            unixtime: uwib,
-            date: wibnix.format("L"),
-            time: wibnix.format("LTS"),
-            time12: wibnix.format("hh:mm"),
-            time12a: wibnix.format("hh:mm a"),
-            time24: wibnix.format("H:mm"),
-            AMPM: wibnix.format("a"),
-            second: wibnix.format("ss"),
-            minute: wibnix.format("mm"),
-            hour: wibnix.format("hh"),
-            weekday: wibnix.format("dddd"),
-            day: wibnix.format("D"),
-            month: wibnix.format("M"),
-            year: wibnix.format("yyyy"),
+            unixtime: unixWib,
+            date: wibMoment.format("L"),
+            time: wibMoment.format("LTS"),
+            time12: wibMoment.format("hh:mm"),
+            time12a: wibMoment.format("hh:mm a"),
+            time24: wibMoment.format("H:mm"),
+            AMPM: wibMoment.format("a"),
+            second: wibMoment.format("ss"),
+            minute: wibMoment.format("mm"),
+            hour: wibMoment.format("hh"),
+            weekday: wibMoment.format("dddd"),
+            day: wibMoment.format("D"),
+            month: wibMoment.format("M"),
+            year: wibMoment.format("yyyy"),
           },
         ],
         wita: [
@@ -81,20 +109,20 @@ module.exports = async (req, res) => {
             timezone: "Asia/NTT",
 
             //Unix Time
-            unixtime: uwita,
-            date: witanix.format("L"),
-            time: witanix.format("LTS"),
-            time12: witanix.format("hh:mm"),
-            time12a: witanix.format("hh:mm a"),
-            time24: witanix.format("H:mm"),
-            AMPM: witanix.format("a"),
-            second: witanix.format("ss"),
-            minute: witanix.format("mm"),
-            hour: witanix.format("hh"),
-            weekday: witanix.format("dddd"),
-            day: witanix.format("D"),
-            month: witanix.format("M"),
-            year: witanix.format("yyyy"),
+            unixtime: unixWita,
+            date: witaMoment.format("L"),
+            time: witaMoment.format("LTS"),
+            time12: witaMoment.format("hh:mm"),
+            time12a: witaMoment.format("hh:mm a"),
+            time24: witaMoment.format("H:mm"),
+            AMPM: witaMoment.format("a"),
+            second: witaMoment.format("ss"),
+            minute: witaMoment.format("mm"),
+            hour: witaMoment.format("hh"),
+            weekday: witaMoment.format("dddd"),
+            day: witaMoment.format("D"),
+            month: witaMoment.format("M"),
+            year: witaMoment.format("yyyy"),
           },
         ],
         wit: [
@@ -103,20 +131,20 @@ module.exports = async (req, res) => {
             timezone: "Asia/Jayapura",
 
             //Unix Time
-            unixtime: uwit,
-            date: witnix.format("L"),
-            time: witnix.format("LTS"),
-            time12: witnix.format("hh:mm"),
-            time12a: witnix.format("hh:mm a"),
-            time24: witnix.format("H:mm"),
-            AMPM: witnix.format("a"),
-            second: witnix.format("ss"),
-            minute: witnix.format("mm"),
-            hour: witnix.format("hh"),
-            weekday: witnix.format("dddd"),
-            day: witnix.format("D"),
-            month: witnix.format("M"),
-            year: witnix.format("yyyy"),
+            unixtime: unixWit,
+            date: witMoment.format("L"),
+            time: witMoment.format("LTS"),
+            time12: witMoment.format("hh:mm"),
+            time12a: witMoment.format("hh:mm a"),
+            time24: witMoment.format("H:mm"),
+            AMPM: witMoment.format("a"),
+            second: witMoment.format("ss"),
+            minute: witMoment.format("mm"),
+            hour: witMoment.format("hh"),
+            weekday: witMoment.format("dddd"),
+            day: witMoment.format("D"),
+            month: witMoment.format("M"),
+            year: witMoment.format("yyyy"),
           },
         ],
 
@@ -125,4 +153,5 @@ module.exports = async (req, res) => {
     }
   });
 };
+
             
