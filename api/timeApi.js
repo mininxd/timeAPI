aab1b78fb9cd7827fbc0054ae27048255168c96f
mininxd <mininsna@gmail.com>
2024-05-27 03:56:04 +0000

    commit

---
+++ api/timeApi.js
@@ -0,0 +1,49 @@
+const moment = require("moment");
+const axios = require("axios");
+
+module.exports = function(req) {
+  return new Promise((resolve, reject) => {
+    axios.get("https://time.akamai.com/") 
+      .then(res => {
+        const time = moment(res.data);
+        const gmtOffset = parseInt(req.query.gmt, 10);
+        if (!gmtOffset) {
+          resolve({
+            "info": "query required",
+            "example": "timeapi.mininxd.my.id?gmt=7"
+          });
+          return;
+        }
+
+        if (gmtOffset >= 13 || gmtOffset <= -13) {
+          resolve({
+            "info": "GMT is exceeded"
+          });
+          return;
+        }
+
+        const newTime = moment.unix(time.unix() + (gmtOffset * 3600));
+        resolve({
+          "gmt": gmtOffset,
+          "hour": newTime.format("HH"),
+          "minute": newTime.format("mm"),
+          "second": newTime.format("ss"),
+          "hour12": newTime.format("hh"),
+          "ampm": newTime.format("A"),
+          "time": newTime.format("HH:mm"),
+          "times": newTime.format("HH:mm:ss"),
+          "time12": newTime.format("hh:mm"),
+          "times12": newTime.format("h:mm:ss"),
+          "day": newTime.format("DD"),
+          "days": newTime.format("dddd"),
+          "month": newTime.format("MM"),
+          "months": newTime.format("MMMM"),
+          "year": newTime.format("YYYY"),
+          "date": newTime.format("L"),
+        });
+      })
+      .catch(err => {
+        reject({ "info": err });
+      });
+  });
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require("path");
+const Fastify = require('fastify');
+const app = Fastify({
+  logger: true
+})
+
+app.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'api'),
+})
+
+
+app.get('/', async (req, res) => {
+const timeApi = require('./api/timeApi.js');
+  const gmtOffset = req.query.gmt;
+  const timeData = await timeApi(req);
+  res.send(timeData);
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+ console.log(`Server running on port : ${port}`);
+});
